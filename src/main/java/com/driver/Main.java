168fda4ae28524142357b1821ffa239dc916fd1f
Suraj Kumar <surajbaranwal56@gmail.com>
2023-01-12 19:49:32 +0530

    polymorphism implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+     static class Product{
+         public int product(int x, int y) {
+             return x*y;
+         }
+         public double product(double x, double y) {
+           return x*y;
+         }
+     }
+     public static  void main(String []args){
+         Product p=new Product();
+         System.out.println(p.product(10,12));
+         System.out.println(p.product(10.00,12.00));
+     }
+
 
 }
