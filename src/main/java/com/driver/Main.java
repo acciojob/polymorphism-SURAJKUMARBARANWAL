145149581803b0e74588c1ecca75c36c6613555d
Suraj Kumar <surajbaranwal56@gmail.com>
2023-01-15 22:38:18 +0530

    overloading done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,22 +3,18 @@ package com.driver;
 public class Main {
      public static  void main(String []args){
          Product p=new Product();
-         p.product(10,12);
-         p.product(12,12,12);
-         p.product(10.00,12.00);
      }
 
+    static  class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
 
-}
-class Product {
-    public int product(int x, int y) {
-        return x * y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-
-    public double product(double x, double y) {
-        return x * y;
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
 }
